45e37909c88d6a91af46cda0156d39096eea99d3
ntiriniga <clarantiriniga@gmail.com>
2016-12-09 10:49:18 +0300

    Init commit

---
+++ server/server.js
@@ -0,0 +1,1 @@
+const path=require('path');const express=require('express');var app=express();const port=process.env.PORT || 3000;const publicPath=path.join(__dirname,'../public');app.use(express.static(publicPath));app.listen(port,function () {    console.log(`Listening to port ${port}`);});
